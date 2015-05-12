1df06fc4437fc185fe247dba9593ed8af76d16d7
yen168 <yenan.chen@baruchmail.cuny.edu>
2015-05-12 15:33:31 -0400

    seed & auto test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "automation"
+
+for i in 1..5
+
+	create_account = User.create([email: "#{i}@#{i}.com", password: "#{i}" * 8, password_confirmation: "#{i}" * 8, name: "#{i}"])
+
+	create_groups = for j in 1..10
+                  		Group.create!([title: "Group no.#{j}", description: "seed no. #{j} group", user_id: "#{i}"])
+                  		for k in 1..5
+                    		Post.create!([group_id: "#{j}",content: "seed no. #{k} post", user_id: "#{i}"])
+                  		end
+                	end
+
+end
